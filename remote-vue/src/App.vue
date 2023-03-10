49c73432037693eecbf018c57ea7acd106d43551
Ankit Pandit(Consultant) <Ankit.Pandit@ril.com>
2023-03-11 01:52:03 +0530

    Angular routes beautified

--- remote-vue/src/App.vue
+++ remote-vue/src/App.vue
@@ -48,11 +48,9 @@ export default defineComponent({
 
 <style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 h1 {
